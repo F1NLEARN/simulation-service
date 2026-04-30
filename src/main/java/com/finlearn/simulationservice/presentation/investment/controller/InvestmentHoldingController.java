27d4e548e71e9224dda431ac25537b4581edcce7
seongxun <rjqlrl30@naver.com>
2026-05-01 03:50:54 +0900

    feat: 내 보유 종목 조회 API 구현

---
+++ src/main/java/com/finlearn/simulationservice/presentation/investment/controller/InvestmentHoldingController.java
@@ -0,0 +1,25 @@
+package com.finlearn.simulationservice.presentation.investment.controller;
+
+import com.finlearn.common.response.CommonResponse;
+import com.finlearn.simulationservice.application.investment.dto.response.HoldingResponse;
+import com.finlearn.simulationservice.application.investment.service.InvestmentHoldingService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/investments/holdings")
+public class InvestmentHoldingController {
+
+    private final InvestmentHoldingService investmentHoldingService;
+
+    @GetMapping
+    public CommonResponse<List<HoldingResponse>> getMyHoldings(@RequestHeader("X-User-Id") String userId) {
+        // TODO: Gateway/JWT 연동 후 X-User-Id 대신 인증 컨텍스트에서 userId를 추출하도록 변경
+        return CommonResponse.success("보유 종목 조회 성공", investmentHoldingService.getMyHoldings(userId));
+    }
+}

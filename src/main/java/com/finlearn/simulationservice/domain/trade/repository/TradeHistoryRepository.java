fc8d87361a26e2d234455ea7a7e3b6b403c74f3d
Lazernes <dbsauddn1@gmail.com>
2026-04-30 12:04:55 +0900

    [refactor] TradeHistory Repository에 JPA 추가

--- src/main/java/com/finlearn/simulationservice/domain/trade/repository/TradeHistoryRepository.java
+++ src/main/java/com/finlearn/simulationservice/domain/trade/repository/TradeHistoryRepository.java
@@ -1,13 +1,12 @@
 package com.finlearn.simulationservice.domain.trade.repository;
 
 import com.finlearn.simulationservice.domain.trade.entity.TradeHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface TradeHistoryRepository {
-
-    TradeHistory save(TradeHistory tradeHistory);
+public interface TradeHistoryRepository extends JpaRepository<TradeHistory, UUID> {
 
     List<TradeHistory> findAllByAccountId(UUID accountId);
 
